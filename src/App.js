878edb190d5f15a1a40040b5aa8b199fc2c05e6a
Harold Ekstrom <hekstrom@liveaction.com>
2021-12-19 15:18:55 -0500

    Draw some arrows

--- src/App.js
+++ src/App.js
@@ -26,14 +26,83 @@ function App() {
     ctx.scale(dpr, dpr)
     ctx.translate(-origin.x, -origin.y)
 
+    const drawLadderArrow = (x1, y1, x2, y2, color) => {
+      const arrowHeadLen = 7
+      const arrowHeadAngle = Math.PI / 8
+      const arrowHeadHypLen = Math.abs(arrowHeadLen / Math.cos(arrowHeadAngle))
+
+      ctx.strokeStyle = color
+      ctx.fillStyle = color
+      ctx.lineWidth = 1 / dpr
+      ctx.beginPath()
+      ctx.moveTo(x1, y1)
+      ctx.lineTo(x2, y2)
+      ctx.stroke()
+
+      const lineAngle = Math.atan2(y2 - y1, x2 - x1)
+      const angle1 = lineAngle + Math.PI + arrowHeadAngle
+      const angle2 = lineAngle + Math.PI - arrowHeadAngle
+
+      // Alternative approach: create a Path2D from an SVG path and transform.
+      ctx.beginPath()
+      ctx.moveTo(x2, y2)
+      ctx.lineTo(x2 + Math.cos(angle1) * arrowHeadHypLen, y2 + Math.sin(angle1) * arrowHeadHypLen)
+      ctx.lineTo(x2 + Math.cos(angle2) * arrowHeadHypLen, y2 + Math.sin(angle2) * arrowHeadHypLen)
+      ctx.closePath()
+      ctx.fill()
+    }
+
+    const drawLadderArrow2 = (x1, y1, x2, y2, color) => {
+      const arrowHeadLen = 7
+      const arrowNotchLen = 5
+      const arrowHeadAngle = (30 * Math.PI) / 180
+      const arrowHeadHypLen = Math.abs(arrowHeadLen / Math.cos(arrowHeadAngle))
+
+      ctx.strokeStyle = color
+      ctx.fillStyle = color
+      ctx.lineWidth = 1 / dpr
+      ctx.beginPath()
+      ctx.moveTo(x1, y1)
+      ctx.lineTo(x2, y2)
+      ctx.stroke()
+
+      const lineAngle = Math.PI + Math.atan2(y2 - y1, x2 - x1)
+      const angle1 = lineAngle + arrowHeadAngle
+      const angle2 = lineAngle - arrowHeadAngle
+
+      ctx.beginPath()
+      ctx.moveTo(x2, y2)
+      ctx.lineTo(x2 + Math.cos(angle1) * arrowHeadHypLen, y2 + Math.sin(angle1) * arrowHeadHypLen)
+      ctx.lineTo(x2 + Math.cos(lineAngle) * arrowNotchLen, y2 + Math.sin(lineAngle) * arrowNotchLen)
+      ctx.lineTo(x2 + Math.cos(angle2) * arrowHeadHypLen, y2 + Math.sin(angle2) * arrowHeadHypLen)
+      ctx.closePath()
+      ctx.fill()
+    }
+
+    const arrowHeadPath = new Path2D("M -7 -4 L 0 0 L -7 4 L -5 0 z")
+    const drawLadderArrow3 = (x1, y1, x2, y2, color) => {
+      ctx.save()
+      ctx.strokeStyle = color
+      ctx.fillStyle = color
+      ctx.lineWidth = 1 / dpr
+      ctx.beginPath()
+      ctx.moveTo(x1, y1)
+      ctx.lineTo(x2, y2)
+      ctx.stroke()
+      ctx.translate(x2, y2)
+      ctx.rotate(Math.atan2(y2 - y1, x2 - x1))
+      ctx.fill(arrowHeadPath)
+      ctx.restore()
+    }
+
     //ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     ctx.fillStyle = "white"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     ctx.strokeStyle = "hsl(218,22%,27%)"
-    ctx.moveTo(0, IMAGE_HEIGHT / 2)
-    ctx.lineTo(IMAGE_WIDTH, IMAGE_HEIGHT / 2)
+    ctx.moveTo(0, IMAGE_HEIGHT / 2 + 0.5)
+    ctx.lineTo(IMAGE_WIDTH, IMAGE_HEIGHT / 2 + 0.5)
     ctx.stroke()
 
     ctx.strokeStyle = "hsl(218,22%,27%)"
@@ -49,6 +118,20 @@ function App() {
 
     ctx.fillStyle = "hsl(354,70%,54%)"
     ctx.fillRect(IMAGE_WIDTH / 2 + 30, IMAGE_HEIGHT - 100 - 10, IMAGE_WIDTH / 2 - 20, 100)
+
+    drawLadderArrow(10, 140.5, 200, 140.5, "darkRed")
+    drawLadderArrow2(10, 150.5, 200, 150.5, "darkGreen")
+    drawLadderArrow3(10, 160.5, 200, 160.5, "purple")
+
+    drawLadderArrow(50.5, 200, 50.5, 300, "darkRed")
+    drawLadderArrow(100.5, 300.5, 80.5, 200.5, "darkRed")
+
+    drawLadderArrow2(200.5, 300.5, 120.5, 200.5, "darkGreen")
+    drawLadderArrow2(220.5, 300, 220.5, 200, "darkGreen")
+
+    drawLadderArrow3(320.5, 300, 320.5, 200, "purple")
+    drawLadderArrow3(240.5, 300.5, 440.5, 200.5, "purple")
+    drawLadderArrow3(250.5, 300, 250.5, 200, "purple")
   }, [width, height, origin])
 
   const onScroll = event => {
